579a92ef7e668555fb044112c6548a291d574122
Brij Kumbhani <brijkumbhani162@gmail.com>
2025-08-23 09:33:38 +0530

    Update portfolio with new features

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Home.css";
-import Me from "../../assets/avatar-1.svg";
+import Me from "../../assets/Brij_Portfolio_image.png";
 import HeaderSocials from "./HeaderSocials";
 import ScrollDown from "./ScrollDown";
 import Shapes from "./Shapes";
